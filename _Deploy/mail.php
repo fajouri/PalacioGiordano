98c5e85a88d4bd1e7832ff3d217a40cdfd8cee78
Yamil Emmanuel Fajouri <fajouri.yamil@gmail.com>
2020-06-17 15:24:19 -0300

    wip

---
+++ _Deploy/mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['InputName'];
+$email = $_POST['InputEmail'];
+$message = $_POST['InputMessage'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "info@palazzogiordano.it";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
